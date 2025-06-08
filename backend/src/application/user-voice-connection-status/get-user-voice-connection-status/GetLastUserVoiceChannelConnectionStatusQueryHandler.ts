72c539025ef6faa75e6ac4598f1871ffc35c8e3e
epouyadou <even.pouyadou@gmail.com>
2025-06-08 18:47:13 +0200

    add voice channel visibility security when displaying logs

--- backend/src/application/user-voice-connection-status/get-user-voice-connection-status/GetLastUserVoiceChannelConnectionStatusQueryHandler.ts
+++ backend/src/application/user-voice-connection-status/get-user-voice-connection-status/GetLastUserVoiceChannelConnectionStatusQueryHandler.ts
@@ -3,7 +3,12 @@ import {
   IUserVoiceChannelStatusRecordsRepository,
   IUserVoiceChannelStatusRecordsRepositorySymbol,
 } from '@domain/voice-channel-status-records/IUserVoiceChannelStatusRecordsRepository';
+import { VoiceChannelStatusRecord } from '@domain/voice-channel-status-records/VoiceChannelStatusRecord';
 import { Inject } from '@nestjs/common';
+import {
+  ICommunicationPlatform,
+  ICommunicationPlatformSymbol,
+} from './../../abstractions/communication-platform/ICommunicationPlatform';
 import {
   GetLastUserVoiceChannelConnectionStatusQuery,
   GetLastUserVoiceChannelConnectionsQueryResult,
@@ -15,12 +20,14 @@ export class GetLastUserVoiceChannelConnectionStatusQueryHandler {
   constructor(
     @Inject(IUserVoiceChannelStatusRecordsRepositorySymbol)
     private readonly repository: IUserVoiceChannelStatusRecordsRepository,
+    @Inject(ICommunicationPlatformSymbol)
+    private readonly communicationPlatform: ICommunicationPlatform,
   ) {}
 
   async handle(
     query: GetLastUserVoiceChannelConnectionStatusQuery,
   ): Promise<GetLastUserVoiceChannelConnectionsQueryResult> {
-    const { guildId, userId, cursor, orderedBy } = query;
+    const { querierId, guildId, userId, cursor, orderedBy } = query;
 
     const records = await this.repository.getFromUserIdAndGuildId(
       guildId,
@@ -30,14 +37,94 @@ export class GetLastUserVoiceChannelConnectionStatusQueryHandler {
       this.QUERY_LIMIT,
     );
 
+    if (records.length === 0) {
+      return {
+        previousCursor: undefined,
+        nextCursor: undefined,
+        userVoiceConnectionStatus: [],
+      };
+    }
+
+    const visibleVoiceChannelCache = new VisibleVoiceChannelSingleCommandCache(
+      this.communicationPlatform,
+    );
+
+    const filteredRecords: VoiceChannelStatusRecord[] = [];
+
+    // Slow but avoid multiple calls to the communication platform to check multiple times the same voice channel visibility
+    // This is a trade-off between performance and complexity.
+    // I don't worry about performance here because the number of records is limited to 10 and this bot is used in a single guild with a limited number of users.
+    for (const record of records) {
+      let hasPermission = true;
+
+      if (record.fromVoiceChannelId) {
+        hasPermission =
+          hasPermission &&
+          (await visibleVoiceChannelCache.isVisible(
+            guildId,
+            record.fromVoiceChannelId,
+            querierId,
+          ));
+      }
+      if (record.toVoiceChannelId) {
+        hasPermission =
+          hasPermission &&
+          (await visibleVoiceChannelCache.isVisible(
+            guildId,
+            record.toVoiceChannelId,
+            querierId,
+          ));
+      }
+
+      if (hasPermission) {
+        filteredRecords.push(record);
+      }
+    }
+
     const previousCursor = query.cursor ? query.cursor - this.QUERY_LIMIT : 0;
     const nextCursor =
-      records.length > 0 ? records[records.length - 1].id : undefined;
+      filteredRecords.length > 0
+        ? filteredRecords[filteredRecords.length - 1].id
+        : undefined;
 
     return {
       previousCursor,
       nextCursor,
-      userVoiceConnectionStatus: records,
+      userVoiceConnectionStatus: filteredRecords,
     };
+  }
+}
+
+class VisibleVoiceChannelSingleCommandCache {
+  private cache: Set<string> = new Set();
+
+  constructor(private readonly communicationPlatform: ICommunicationPlatform) {}
+
+  async isVisible(
+    guildId: string,
+    channelId: string,
+    userId: string,
+  ): Promise<boolean> {
+    const cacheKey = `${guildId}-${channelId}-${userId}`;
+    if (this.cache.has(cacheKey)) {
+      return true;
+    }
+
+    const hasPermission =
+      await this.communicationPlatform.hasPermissionToAccessTheVoiceChannel(
+        guildId,
+        channelId,
+        userId,
+      );
+
+    if (hasPermission) {
+      this.cache.add(cacheKey);
+    }
+
+    return hasPermission;
+  }
+
+  clearCache() {
+    this.cache.clear();
   }
 }
